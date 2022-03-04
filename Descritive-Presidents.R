03cdc90554a7088bbd96fc508313802c678faf8c
RahulGhulanawar <100698484+RahulGhulanawar@users.noreply.github.com>
2022-03-04 20:35:29 +0530

    Add files via upload

---
+++ Descritive-Presidents.R
@@ -0,0 +1,25 @@
+##Descriptive statistics-AirPassengers
+
+
+air<- datasets::presidents
+
+
+head(air)
+
+tail(air)
+
+
+summary(air)
+
+head(air)
+
+plot(air)
+
+plot(air, type='l')
+
+boxplot(air)
+
+boxplot.stats(air)$out
+
+hist(air, col='red')
+
